4073e4c1553d8d2c90a571c20adab7c23af04226
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-11-20 19:20:12 +0200

    [add] added command /start to bot

--- bot.js
+++ bot.js
@@ -8,3 +8,18 @@ const {
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const { connectDatabase, users } = require('./db.js');
 connectDatabase();
+let obj;
+let tasksList;
+bot.start(async (ctx) => {
+  await ctx.reply(`Привет ${ctx.message.from.first_name}, этот бот создан для планировки задач.\nНапиши команду /help, чтобы узнать команды бота.`);
+  const userExists = await users.findOne({ username: ctx.message.from.username });
+  if (userExists === null) users.insertOne({ username: `${ctx.message.from.username}`, chatId: `${ctx.chat.id}`, tasks: [] });
+  obj = await users.findOne({ chatId: String(ctx.chat.id) });
+  tasksList = obj.tasks;
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
