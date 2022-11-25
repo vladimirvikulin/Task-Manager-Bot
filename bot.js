ef419b4682234f14b809ff335fb96d91660c07ce
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-11-25 11:33:53 +0200

    [add] added new button and emojis

--- bot.js
+++ bot.js
@@ -63,9 +63,10 @@ bot.command('updateTask', async (ctx) => {
 bot.command('menu', async (ctx) => {
   await ctx.replyWithHTML('<b>Меню планировщика</b>', Markup.inlineKeyboard(
     [
-      [Markup.button.callback('Мои задачи', 'myTasks')],
-      [Markup.button.callback('Добавить задачу', 'addTask')],
-      [Markup.button.callback('Удалить задачу', 'deleteTask')],
+      [Markup.button.callback('Мои задачи 📋', 'myTasks')],
+      [Markup.button.callback('Добавить задачу ✏️', 'addTask')],
+      [Markup.button.callback('Удалить задачу 🗑️', 'deleteTask')],
+      [Markup.button.callback('Обновить статус задачи 🔃', 'updateTask')],
     ]
   ));
 });
